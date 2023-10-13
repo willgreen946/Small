f829c3ba7a38efdf96fc91d8b8cc58d20b183718
Will Green <72352225+willgreen946@users.noreply.github.com>
2023-10-13 10:43:04 +0100

    Add files via upload

--- BinaryConverterSharp/main.cs
+++ BinaryConverterSharp/main.cs
@@ -1,1 +1,31 @@
+using System;
 
+/* Simple interger to binary converter in C# */
+
+namespace BinaryConverter
+{
+ class BC
+ {
+	static void Main (string[] args)
+	{
+	 int num = 0;
+	 string bin;
+
+	 foreach (string arg in args)
+	 {
+		if (int.TryParse(arg, out num))
+		{
+		 /* Converts to base 2 */
+		 bin = Convert.ToString(num, 2);
+		 Console.WriteLine(bin);
+		}
+
+		else
+		{
+		 Console.WriteLine("Cannot convert " + arg + " to an interger!\nstopping ...");
+		 break;
+		}
+	 }
+	}
+ }
+}
